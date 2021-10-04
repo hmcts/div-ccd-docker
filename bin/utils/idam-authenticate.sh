a2d0b220673eb50ab60466122aff909d56407399
danielbe11 <daniel.bell@solirius.com>
2021-10-04 16:15:01 +0100

    Changes 1

--- bin/utils/idam-authenticate.sh
+++ bin/utils/idam-authenticate.sh
@@ -4,6 +4,13 @@ set -eu
 
 USERNAME=${1:-fr_applicant_sol@sharklasers.com}
 PASSWORD=${2:-Testing1234}
-IDAM_API_BASE_URL=${IDAM_STUB_LOCALHOST:-https://idam-api.aat.platform.hmcts.net}
 
-curl --show-error --header 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: application/json' -d "username=${USERNAME}&password=${PASSWORD}" "${IDAM_API_BASE_URL:-http://localhost:5000}/loginUser" | docker run --rm --interactive stedolan/jq -r .api_auth_token
+if [ "${ENVIRONMENT:-local}" == "local" ]; then
+  IDAM_API_BASE_URL=${IDAM_STUB_LOCALHOST:-http://localhost:5000}
+elif [ "$ENVIRONMENT" == "aat" ]; then
+  IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net
+else
+  echo "Set your ENVIRONMENT variable to 'aat' or 'local'"
+fi
+
+curl --show-error --header 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: application/json' -d "username=${USERNAME}&password=${PASSWORD}" "$IDAM_API_BASE_URL/loginUser" | docker run --rm --interactive stedolan/jq -r .api_auth_token
--- bin/utils/idam-user-token.sh
+++ bin/utils/idam-user-token.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
-IMPORTER_USERNAME=${1:-fr_applicant_sol@sharklasers.com}
-IMPORTER_PASSWORD=${2:-Testing1234}
-IDAM_URI=${IDAM_STUB_LOCALHOST:-https://idam-api.aat.platform.hmcts.net}
-REDIRECT_URI="https://div-pfe-aat.service.core-compute-aat.internal/authenticated"
-CLIENT_ID="divorce"
-CLIENT_SECRET="thUphEveC2Ekuqedaneh4jEcRuba4t2t"
+if [ "${ENVIRONMENT:-local}" == "local" ]; then
+  IMPORTER_USERNAME=${1:-ccd.docker.default@hmcts.net}
+  IMPORTER_PASSWORD=${2:-Pa55word11}
+  IDAM_URI=${IDAM_STUB_LOCALHOST:-http://localhost:5000}
+  REDIRECT_URI="http://localhost:3451/oauth2redirect"
+  CLIENT_ID="ccd_gateway"
+  CLIENT_SECRET="ccd_gateway_secret"
+elif [ "$ENVIRONMENT" == "aat" ]; then
+  IMPORTER_USERNAME=${1:-fr_applicant_sol@sharklasers.com}
+  IMPORTER_PASSWORD=${2:-Testing1234}
+  IDAM_URI=https://idam-api.aat.platform.hmcts.net
+  REDIRECT_URI="https://div-pfe-aat.service.core-compute-aat.internal/authenticated"
+  CLIENT_ID="divorce"
+  CLIENT_SECRET="thUphEveC2Ekuqedaneh4jEcRuba4t2t"
+else
+  echo "Set your ENVIRONMENT variable to 'aat' or 'local'"
+fi
+
 CURL_OPTS="$CURL_OPTS -S --silent"
 code=$(curl ${CURL_OPTS} -u "${IMPORTER_USERNAME}:${IMPORTER_PASSWORD}" -XPOST "${IDAM_URI}/oauth2/authorize?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=${CLIENT_ID}" -d "" | jq -r .code)
 
